c7b7f53a8fc532a66298a0575ed6201add004ffe
camila-06R <yorely.romero@unl.edu.ec>
2024-06-02 16:11:57 -0500

    Create  README.c

---
+++ README.c
@@ -0,0 +1,107 @@
+# Taller de Programación en C 
+Este repositorio contiene las soluciones a los ejercicios del taller de programación en C utilizando los bucles for, while y do while. 
+
+## Estructura del Repositorio 
+Cada ejercicio tiene tres versiones, una con for, otra con while y otra con do while. A continuación se detalla la ubicación de cada uno de los ejercicios: 
+
+1. Suma de los dígitos de un número 
+- for: ejercicios/suma-digitos/for.c 
+- while: ejercicios/suma-digitos/while.c 
+- do while: ejercicios/suma-digitos/do_while.c 
+
+2. Inverso de un número 
+- for: ejercicios/inverso-numero/for.c 
+- while: ejercicios/inverso-numero/while.c 
+- do while: ejercicios/inverso-numero/do_while.c 
+
+3. Potencia mediante sumas sucesivas 
+- for: ejercicios/potencia-numero/for.c 
+- while: ejercicios/potencia-numero/while.c 
+- do while: ejercicios/potencia-numero/do_while.c 
+
+4. Radicacion mediante restas sucesivas 
+- for: ejercicios/radicacion-numero/for.c 
+- while: ejercicios/radicacion-numero/while.c 
+- do while: ejercicios/radicacion-numero/do_while.c 
+
+5. Serie de Fibonacci 
+- for: ejercicios/serie-fibonacci/for.c 
+- while: ejercicios/serie-fibonacci/while.c 
+- do while: ejercicios/serie-fibonacci/do_while.c 
+
+6. Factorial de un numero 
+- for: ejercicios/factorial-numero/for.c 
+- while: ejercicios/factorial-numero/while.c 
+- do while: ejercicios/factorial-numero/do_while.c 
+
+7. Suma de una serie aritmetica 
+- for: ejercicios/serie-aritmetica/for.c 
+- while: ejercicios/serie-aritmetica/while.c 
+- do while: ejercicios/serie-aritmetica/do_while.c 
+
+8. Producto de una serie geometrica 
+- for: ejercicios/producto-serie-geometrica/for.c 
+- while: ejercicios/producto-serie-geometrica/while.c 
+- do while: ejercicios/producto-serie-geometrica/do_while.c 
+
+9. Numero de digitos de un numero 
+- for: ejercicios/digitos-numero/for.c 
+- while: ejercicios/digitos-numero/while.c 
+- do while: ejercicios/digitos-numero/do_while.c 
+
+10. Verificar si un numero es primo 
+- for: ejercicios/verificar-numero-primo/for.c 
+- while: ejercicios/verificar-numero-primo/while.c 
+- do while: ejercicios/verificar-numero-primo/do_while.c 
+
+
+11. Encontrar el Minimo Comun Divisor 
+- for: ejercicios/minimo-comun-divisor/for.c 
+- while: ejercicios/minimo-comun-divisor/while.c 
+- do while: ejercicios/minimo-comun-divisor/do_while.c 
+
+12. Sumar los numeros impares hasta n 
+- for: ejercicios/sumar-numero-impares/for.c 
+- while: ejercicios/sumar-numero-impares/while.c 
+- do while: ejercicios/sumar-numero-impares/do_while.c 
+
+13. Sumar los numeros pares hasta n 
+- for: ejercicios/sumar-numero-par/for.c 
+- while: ejercicios/sumar-numero-par/while.c 
+- do while: ejercicios/sumar-numero-par/do_while.c 
+
+14. Suma de los cuadrados los primeros n numeros 
+- for: ejercicios/suma-cuadrados/for.c 
+- while: ejercicios/suma-cuadrados/while.c 
+- do while: ejercicios/suma-cuadrados/do_while.c 
+
+15. Suma de los cubos de los primeros n numeros 
+- for: ejercicios/suma-cubos/for.c 
+- while: ejercicios/suma-cubos/while.c 
+- do while: ejercicios/suma-cubos/do_while.c 
+
+16. Suma de una serie de numeros fraccionarios 
+- for: ejercicios/sum-serie-numeros-fraccionarios/for.c 
+- while: ejercicios/sum-serie-numeros-fraccionario/while.c 
+- do while: ejercicios/sum-serie-numeros-fraccionario/do_while.c 
+
+17. Producto de una serie de numeros fraccionarios 
+- for: ejercicios/producto-serie-numeros-fraccionarios/for.c 
+- while: ejercicios/producto-serie-numeros-fraccionarios/while.c 
+- do while: ejercicios/producto-serie-numeros-fraccionarios/do_while.c 
+
+18. Contar los numeros primos hasta n 
+- for: ejercicios/contar-numero-primos/for.c 
+- while: ejercicios/contar-numero-primos/while.c 
+- do while: ejercicios/contar-numero-primos/do_while.c 
+
+19. Calcular el n-ésimo número triangular 
+- for: ejercicios/calcular-n-ésimo-número-triangular/for.c 
+- while: ejercicios/calcular-n-ésimo-número-triangular/while.c 
+- do while: ejercicios/calcular-n-ésimo-número-triangular/do_while.c 
+
+
+20. Calcular la suma de los factoriales de los primeros n números 
+- for: ejercicios/suma_factoriales/for.c 
+- while: ejercicios/suma_factoriales/while.c 
+- do while: ejercicios/suma_factoriales/do_while.c
